319d5211c60f5f9b0b9391e517c097f969671b61
Danny Hermes <daniel.j.hermes@gmail.com>
2018-11-05 12:42:59 -0800

    Ran `black==18.9b0` code-formatter over code.
    Done via
    
    $ black --line-length=79 $(git ls-files -- '*py')

--- sphinx_docstring_typing/__init__.py
+++ sphinx_docstring_typing/__init__.py
@@ -22,25 +22,34 @@ import sphinx.util.logging
 _LOGGER = sphinx.util.logging.getLogger(__name__)
 
 # All available type hints from the typing module.
-_TYPES = set(typing.__all__) - set((
-    'cast', 'get_type_hints', 'NewType', 'no_type_check',
-    'no_type_check_decorator', 'overload', 'TYPE_CHECKING'))
+_TYPES = set(typing.__all__) - set(
+    (
+        "cast",
+        "get_type_hints",
+        "NewType",
+        "no_type_check",
+        "no_type_check_decorator",
+        "overload",
+        "TYPE_CHECKING",
+    )
+)
 
 # A pattern that matches any available type hint.
-_TYPE_PATTERN = '|'.join(_TYPES)
+_TYPE_PATTERN = "|".join(_TYPES)
 
 # Regex used to find & replace type hints.
-TYPE_RE = re.compile(r'\*?(({})\[.+\])\*?'.format(_TYPE_PATTERN))
+TYPE_RE = re.compile(r"\*?(({})\[.+\])\*?".format(_TYPE_PATTERN))
 
 # Bare type hints don't have any arguments, e.g., Any vs List[int].
-_BARE_TYPES = ('Any', 'Text', 'Hashable', 'Sized', 'ByteString', 'AnyStr')
+_BARE_TYPES = ("Any", "Text", "Hashable", "Sized", "ByteString", "AnyStr")
 
 # A pattern that matches any bare types.
-_BARE_TYPES_PATTERN = '|'.join(_BARE_TYPES)
+_BARE_TYPES_PATTERN = "|".join(_BARE_TYPES)
 
 # Regex used to find & replace bare type hints.
 BARE_TYPE_RE = re.compile(
-    r'\*?({})(?! ?\[)(?! ?`)\*?'.format(_BARE_TYPES_PATTERN))
+    r"\*?({})(?! ?\[)(?! ?`)\*?".format(_BARE_TYPES_PATTERN)
+)
 
 
 class CollapseAttrsVisitor(ast.NodeTransformer):
@@ -63,16 +72,18 @@ class CollapseAttrsVisitor(ast.NodeTransformer):
         Name(id='google.auth.credentials.Credentials')
 
     """
+
     def visit_Attribute(self, node):
         child = self.visit(node.value)
         parent_attr = (
-            child.attr if isinstance(child, ast.Attribute) else child.id)
-        return ast.Name(id=parent_attr + '.' + node.attr)
+            child.attr if isinstance(child, ast.Attribute) else child.id
+        )
+        return ast.Name(id=parent_attr + "." + node.attr)
 
 
 class RedocVisitor(ast.NodeVisitor):
     def __init__(self):
-        self.output = ''
+        self.output = ""
 
     def visit_Subscript(self, node):
         self.visit(node.value)
@@ -84,35 +95,34 @@ class RedocVisitor(ast.NodeVisitor):
     def visit_Tuple(self, node):
         for idx, elt in enumerate(node.elts):
             if idx:
-                self.output += ', '
+                self.output += ", "
             self.visit(elt)
 
     def visit_Index(self, node):
-        self.output += ' [ '
+        self.output += " [ "
         self.generic_visit(node)
-        self.output += ' ] '
+        self.output += " ] "
 
     def visit_Name(self, node):
         name = node.id
-        ref = 'py:obj'
+        ref = "py:obj"
 
         if hasattr(typing, name):
-            name = '~typing.' + name
+            name = "~typing." + name
 
-        self.output += ':{ref}:`{name}`'.format(
-            ref=ref, name=name)
+        self.output += ":{ref}:`{name}`".format(ref=ref, name=name)
 
     def visit_Attribute(self, node):
         self.visit(node.value)
-        self.output += '.' + node.attr
+        self.output += "." + node.attr
 
     def visit_UnaryOp(self, node):
         if isinstance(node.op, ast.Invert):
-            node.operand.id = '~' + node.operand.id
+            node.operand.id = "~" + node.operand.id
         return self.generic_visit(node)
 
     def visit_Ellipsis(self, node):
-        self.output += '...'
+        self.output += "..."
 
 
 def transform(annotation):
@@ -125,8 +135,7 @@ def transform(annotation):
     return visitor.output
 
 
-def autodoc_process_docstring(
-        app, what, name, obj, options, lines):
+def autodoc_process_docstring(app, what, name, obj, options, lines):
     """Replaces PEP 484 style type annotations in docstrings with sphinx-style
     references."""
 
@@ -138,28 +147,28 @@ def autodoc_process_docstring(
         try:
             # Check for type hints that take arguments.
             new_line = re.sub(
-                TYPE_RE,
-                lambda match: transform(match.group(1)),
-                new_line)
+                TYPE_RE, lambda match: transform(match.group(1)), new_line
+            )
 
             # Check for any lingering bare type hints.
             new_line = re.sub(
-                BARE_TYPE_RE,
-                lambda match: transform(match.group(1)),
-                new_line)
+                BARE_TYPE_RE, lambda match: transform(match.group(1)), new_line
+            )
 
         except Exception as e:
             _LOGGER.warn(
-                'sphinx-docstring-typing: Un-parseable line in docstring: \n'
-                '\t> %s \n\nException: %s' % (line, e))
+                "sphinx-docstring-typing: Un-parseable line in docstring: \n"
+                "\t> %s \n\nException: %s" % (line, e)
+            )
 
         if line != new_line:
             _LOGGER.verbose(
-                'sphinx-docstring-typing: docstring line for %s replaced: '
-                '\n\t> %s \n\t> %s' % (name, line, new_line))
+                "sphinx-docstring-typing: docstring line for %s replaced: "
+                "\n\t> %s \n\t> %s" % (name, line, new_line)
+            )
             lines[n] = new_line
 
 
 def setup(app):
     """Sphinx plug-in entrypont."""
-    app.connect('autodoc-process-docstring', autodoc_process_docstring)
+    app.connect("autodoc-process-docstring", autodoc_process_docstring)
